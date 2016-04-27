3f479f0bf31dc1d83e68e58fb6aac77eab47adda
Mangalesh <mangalesh2006@gmail.com>
2016-04-27 16:40:05 +0200

    page.tpl update2

--- sites/all/themes/freesample/templates/page.tpl.php
+++ sites/all/themes/freesample/templates/page.tpl.php
@@ -79,7 +79,7 @@
         	<div class="page">
             	<div class="col1">
 					<?php if ($logo): ?>
-                        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+                        <a href="<?php print $front_page; ?>" title="<?php print t('click Home'); ?>" rel="home" id="logo">
                             <!-- <img src="<?php //print $logo; ?>" alt="<?php //print t('Home'); ?>" /> -->
                         </a>
                     <?php endif; ?>
